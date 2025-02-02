f98658710a3a50c76083f3f6ffbc6c566616121e
Yuvangates <yuvangates.dake@gmail.com>
2025-02-02 13:18:40 +0530

    userAnalytics page latest

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
-import Dashboard from "./components/dashboard.jsx";
-import Navbar from "./components/navbar.jsx";
+import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
+import AuthPage from "./pages/AuthPage";
+import { Toaster } from "react-hot-toast";
 import "./App.css";
+import { AuthProvider } from "./context/AuthContext";
+import UserDashboard from "./pages/Dashboard2";
 
 function App() {
   return (
-    <>
-      <Navbar></Navbar>
-      <Dashboard></Dashboard>
-    </>
+    <AuthProvider>
+      <Toaster />
+      <Router>
+        <Routes>
+          <Route path="/" element={<UserDashboard />} />
+          {/* <Route path="/" element={<Dashboard />} /> */}
+          <Route path="/auth" element={<AuthPage />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
